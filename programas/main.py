5d1b5ab6975978a2e45f6a28993e5b14b3fa3f7d
Ellie de Oliveira <ellie.a.oliveira@gmail.com>
2024-06-06 23:22:10 -0300

    feat(BFG-16): adicionar selects para cada tabela

--- programas/main.py
+++ programas/main.py
@@ -2,40 +2,52 @@ import oracledb
 import pandas as pd
 
 
-def main():
-    conexao, inst_SQL, conn = conecta_BD()
-    opc = 0
-    while (opc != 3 and conexao == True):
-        print("1-Cadastro de informações contidas no CSV")
-        print("3-Sair")
+entidade_fields = [
+    "entidade_id",
+    "nome",
+    "codigo",
+    "grupo"
+]
 
-        opc = int(input("Digite a opcao (1-3): "))
+poluicao_cidade_fields = [
+    "cidade",
+    "regiao",
+    "ano",
+    "qualidade_ar",
+    "poluicao_agua",
+    "entidade_id"
+]
 
-        if (opc == 1):
+producao_plastico_fields = [
+    "ano",
+    "producao_anual_plastico",
+    "partici_emissao_oceanos",
+    "partici_reciclagem_regional",
+    "partici_queima_regional",
+    "partici_lixo_mal_gerido",
+    "partici_aterros_regional",
+    "lixo_mal_gerido_per_capita",
+    "entidade_id"
+]
 
-            try:
-                tabela_oracle = manipulating_dataframe_to_insert()[0]
-                df_final = manipulating_dataframe_to_insert()[1]
+pertence_fields = [
+    "entidade_id",
+    "entidade_pertence"
+]
 
-                str_insert = f"""INSERT INTO {tabela_oracle} (nome, codigo, grupo) VALUES (:1, :2, :3)"""
+usuario_fields = [
+    "nome_usuario",
+    "nome",
+    "senha"
+]
 
-                cursor = conn.cursor()
-
-                try:
-                    for index, row in df_final.iterrows():
-                        cursor.execute(str_insert, (row['Entidade'], row['Código'], row['grupo']))
-
-                    conn.commit()
-                except Exception as error:
-                    print(f"Erro ao inserir os dados: {error}")
-                finally:
-                    cursor.close()
-                    conn.close()
-
-            except Exception as error:
-                print(f"Erro ao inserir os dados: {error}")
-            finally:
-                print("Operação realizada com sucesso!\n")
+tables = {
+    "entidade": entidade_fields,
+    "poluicao_cidade": poluicao_cidade_fields,
+    "producao_plastico": producao_plastico_fields,
+    "pertence": pertence_fields,
+    "usuario": usuario_fields
+}
 
 
 def conecta_BD():
@@ -43,31 +55,115 @@ def conecta_BD():
         # conectar com o Servidor
         dnStr = oracledb.makedsn("oracle.fiap.com.br", "1521", "ORCL")
         # efetuar a conexao com o usuario
-        conn = oracledb.connect(user='rm553243', password='180600', dsn=dnStr)
-
-        # Criar as instrucoes para cada modulo
-        inst_SQL = conn.cursor()
+        conn = oracledb.connect(user='rm552824', password='280200', dsn=dnStr)
 
     except Exception as e:
         print("Erro: ", e)
         conexao = False
-        inst_SQL = ""
         conn = ""
     else:
         conexao = True
 
-    return (conexao, inst_SQL, conn)
+    return conexao, conn
 
 
-def insert_tabela(inst_SQL, conn, str_insert):
+def select(cursor, table_name, fields="*", where=None, join=None, order_by=None, group_by=None):
+    query = f"SELECT {fields} FROM {table_name}"
+    
+    if join:
+        query += f" {join}"
+    
+    if where:
+        query += f" WHERE {where}"
+        
+    if order_by:
+        query += f" ORDER BY {order_by}"
+        
+    if group_by:
+        query += f" GROUP BY {group_by}"
+    
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def inserir_arquivos_csv(conn):
     try:
-        inst_SQL.execute(str_insert)
-        conn.commit()
+        tabela_oracle = manipulating_dataframe_to_insert()[0]
+        df_final = manipulating_dataframe_to_insert()[1]
 
-    except:
-        print("Erro de transacao com o BD")
-    else:
-        print("Dados gravados com sucesso")
+        str_insert = f"""INSERT INTO {tabela_oracle} (nome, codigo, grupo) VALUES (:1, :2, :3)"""
+
+        cursor = conn.cursor()
+
+        try:
+            for _, row in df_final.iterrows():
+                cursor.execute(str_insert, (row['Entidade'], row['Código'], row['grupo']))
+
+            conn.commit()
+        except Exception as error:
+            print(f"Erro ao inserir os dados: {error}")
+        finally:
+            cursor.close()
+            conn.close()
+
+    except Exception as error:
+        print(f"Erro ao inserir os dados: {error}")
+    finally:
+        print("Operação realizada com sucesso!\n")
+
+
+def print_table(table_name, fields, data):
+    col_widths = [max(len(str(item)) for item in col) for col in zip(*([fields] + data))]
+
+    def print_separator():
+        print("+" + "+".join("-" * (width + 2) for width in col_widths) + "+")
+
+    def print_row(row):
+        print("| " + " | ".join((str(val) + " " * (width - len(str(val)))) for val, width in zip(row, col_widths)) + " |")
+
+    print(f"Tabela: {table_name}")
+    print_separator()
+    print_row(fields)
+    print_separator()
+    for row in data:
+        print_row(row)
+    print_separator()
+    print()
+    
+
+def main():
+    conexao, conn = conecta_BD()
+    opc = -1
+    while (opc != 0 and conexao == True):
+        print("1-Cadastro de informações contidas no CSV")
+        print("2-Listar todas as entidades")
+        print("3-Listar todas as linhas referentes à poluição na cidade")
+        print("4-Listar todas as linhas referentes à produção de plástico")
+        print("5-Listar todos os usuários")
+        print("0-Sair\n")
+
+        opc = int(input("Digite a opção desejada, ou 0 para sair: "))
+
+        if (opc == 1):
+            inserir_arquivos_csv(conn)
+        elif (opc == 2):
+            resposta = select(conn.cursor(), "entidade")
+            print_table("Entidade", tables["entidade"], resposta)
+        elif (opc == 3):
+            resposta = select(conn.cursor(), "poluicao_cidade")
+            print_table("Poluição na Cidade", tables["poluicao_cidade"], resposta)
+        elif (opc == 4):
+            resposta = select(conn.cursor(), "producao_plastico")
+            print_table("Produção de Plastico", tables["producao_plastico"], resposta)
+        elif (opc == 5):
+            resposta = select(conn.cursor(), "usuario")
+            print_table("Usuário", tables["usuario"], resposta)
+        elif (opc == 0):
+            print("Saindo...")
+        else:
+            print("Opção inválida. Tente novamente.")
+            
+        print("\n")
 
 
 def manipulating_dataframe_to_insert():
@@ -99,15 +195,9 @@ def manipulating_dataframe_to_insert():
     df_entidade['grupo'] = df_entidade['Código'].apply(definir_grupo)
 
     df_entidade = df_entidade.reset_index()
-
-    coluna_entidade = df_entidade['Entidade']
-    coluna_codigo = df_entidade['Código']
-    coluna_grupo = df_entidade['grupo']
-
     tabela_oracle = 'entidade'
 
     return [tabela_oracle, df_entidade]
-
 
 
 if (__name__ == "__main__"):
---
+++ run.sh
@@ -0,0 +1,4 @@
+python3 -m venv .venv
+python3 -m pip install -r requirements.txt
+clear >$(tty)
+python3 ./programas/main.py
